320cde08b4e6d3239c200364465d861f52b2854c
Andrew <andrewxie@dpengineering.org>
2019-12-06 12:04:50 -0800

    Add some basic pseudocode for when the odrive is implemented.

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+import cv2
+
 from .Kinect_Skeleton_Tracker import Tracker
+
+
+t = Tracker()
+t.run()
+
+f = t.getFrame()
+while f is None:
+    f = t.getFrame()
+
+while True:
+    f = t.getFrame()
+    print(t.calculate_angle("RIGHT_HAND", "LEFT_HAND"))
+    print("Frame gotten!")
+    cv2.imshow("img", f)
+    print("Frame shown!")
+    if (cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+    '''
+    if angle is not None:
+                if t.returnY("LEFT_HAND") < t.returnY("LEFT_ELBOW") and t.returnY("RIGHT_HAND") < t.returnY("RIGHT_ELBOW"):
+                    print("Got angle:", angle)
+                    #sw.updateTimer(firstTime,timer_on)
+                    #self.kmm.set_velocity(self.kmm.adjust_angle(angle))
+                else:
+                    print("Hands not above elbow")
+            else:
+                #self.kmm.set_velocity(self.kmm.ramp_down())
+                print("No user detected")
+                #sw.updateTimer(firstTime,timer_on)
+    '''
+t.stop()
+cv2.destroyAllWindows()
